2d4129525a1cd91109a8fbe22eaa2aac6e08efc3
Sethiepooh <sethwalters2005@outlook.com>
2025-12-08 18:05:01 -0800

    update

--- Assets/Scripts/Universal Scripts/BPMInteract.cs
+++ Assets/Scripts/Universal Scripts/BPMInteract.cs
@@ -30,7 +30,7 @@ public class BPMInteract : MonoBehaviour
             interval.CheckForNewInterval(sampledTime);           
         }
 
-        //ResetMovement();
+        ResetMovement();
 
         Check();
     }
