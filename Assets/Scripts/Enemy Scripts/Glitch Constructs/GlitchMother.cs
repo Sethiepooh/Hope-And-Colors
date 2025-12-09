d0ffea8b936d2a769bc9755c3ab6d6423f5b1f65
Sethiepooh <82126052+Sethiepooh@users.noreply.github.com>
2025-12-09 02:23:03 -0800

    udate

--- Assets/Scripts/Enemy Scripts/Glitch Constructs/GlitchMother.cs
+++ Assets/Scripts/Enemy Scripts/Glitch Constructs/GlitchMother.cs
@@ -1,8 +1,6 @@
-using TMPro;
+
 using UnityEngine;
-using UnityEngine.LightTransport;
-using static UnityEngine.GraphicsBuffer;
-using UnityEngine.ParticleSystemJobs;
+
 
 public class GlitchMother : EnemyBase
 {
